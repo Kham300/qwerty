932bce12dcc8901b26eb4ac6daee9447c02cdd9a
kuznetsov_as <kuznetsov_as@ursip.ru>
2018-08-13 11:07:28 +0300

    #6 Контроллеры

---
+++ src/main/kotlin/ru/ursip/webservice/mgsn/workplace/controller/ApiInfoController.kt
@@ -0,0 +1,25 @@
+package ru.ursip.webservice.mgsn.workplace.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import springfox.documentation.annotations.ApiIgnore
+
+@RequestMapping(path = ["v1"], produces = ["application/json; charset=utf-8"])
+@Controller
+@CrossOrigin
+@ApiIgnore
+class ApiInfoController {
+
+    /**
+     * Редирект на страницу сваггера с /v1/
+     */
+    @GetMapping
+    fun getInfo() =
+            try {
+                "redirect:/swagger-ui.html"
+            } catch (ex: Exception) {
+                ex.message
+            }
+}
---
+++ src/main/kotlin/ru/ursip/webservice/mgsn/workplace/controller/OrganizationFizController.kt
@@ -0,0 +1,72 @@
+package ru.ursip.webservice.mgsn.workplace.controller
+
+import io.swagger.annotations.*
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import ru.ursip.webservice.mgsn.workplace.model.OrganizationFiz
+import ru.ursip.webservice.mgsn.workplace.model.OrganizationFizProjection
+import ru.ursip.webservice.mgsn.workplace.service.OrganizationFizService
+import java.util.*
+
+@Api(tags = ["Работа с физ. лицами"])
+@RestController
+@RequestMapping("v1/OrganizationFiz", produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
+@CrossOrigin
+class OrganizationFizController(private val organizationFizService: OrganizationFizService) {
+
+    @ApiImplicitParams(ApiImplicitParam(name = "page", dataType = "integer", paramType = "query"),
+            ApiImplicitParam(name = "size", dataType = "integer", paramType = "query"),
+            ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query"))
+    @ApiOperation("Получение физ. лиц")
+    @PostMapping("/getAll")
+    fun getAll(@ApiParam("фильтр", required = false) filter: OrganizationFizProjection? = null,
+               @PageableDefault pageable: Pageable): ResponseEntity<Any> {
+        val organizations = organizationFizService.getAll(filter, pageable)
+        return if (organizations.hasContent()) ResponseEntity.ok(organizations)
+        else ResponseEntity.noContent().build<Any>()
+    }
+
+    @ApiOperation("Получение физ. лиц для витрины")
+    @GetMapping("/getAllProjection")
+    fun getAllProjection(@ApiParam("фильтр", required = false) filter: OrganizationFizProjection? = null,
+                         @PageableDefault pageable: Pageable): ResponseEntity<Any> {
+        val organizations = organizationFizService.getAllProjection(filter, pageable)
+        return if (organizations.hasContent()) ResponseEntity.ok(organizations)
+        else ResponseEntity.noContent().build<Any>()
+    }
+
+    @ApiOperation("Получение физ. лиц для других модулей")
+    @GetMapping("/getAllExternal")
+    fun getAllExternal(@ApiParam("Строка для поиска") @RequestParam search: String, @PageableDefault pageable: Pageable): ResponseEntity<Any> {
+        val organizations = organizationFizService.getAllExternal(search, pageable)
+        return if (organizations.isNotEmpty()) ResponseEntity.ok(organizations)
+        else ResponseEntity.noContent().build<Any>()
+    }
+
+    @ApiOperation("Получение физ. лиц по идентификатору")
+    @GetMapping("/get/{id}")
+    fun getById(@ApiParam("Идентификатор") @PathVariable id: UUID): ResponseEntity<Any> {
+        val organization = organizationFizService.getById(id)
+        return if (organization != null) ResponseEntity.ok(organization)
+        else ResponseEntity.noContent().build<Any>()
+    }
+
+    @ApiOperation("Создание физ. лица")
+    @PostMapping("/create")
+    fun create(@ApiParam("Физ.лицо") @RequestBody organizationFiz: OrganizationFiz): ResponseEntity<Any> =
+            ResponseEntity.ok().body(organizationFizService.create(organizationFiz))
+
+    @ApiOperation("Обноваление физ. лица")
+    @PostMapping("/update")
+    fun update(@ApiParam("Физ.лицо") @RequestBody organizationFiz: OrganizationFiz): ResponseEntity<Any> =
+            ResponseEntity.ok().body(organizationFizService.update(organizationFiz))
+
+    @ApiOperation("Удаление физ. лица")
+    @PostMapping("/delete")
+    fun delete(@ApiParam("Идентификатор") @RequestParam id: UUID) = organizationFizService.deleteById(id)
+
+
+}
